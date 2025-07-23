79c4dc293e5c4a6314cd6c77267b22fce1d80c96
yashwanthvarma5 <118748767+yashwanthvarma5@users.noreply.github.com>
2025-07-23 10:51:06 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://moodbackend-w2s8.onrender.com/api",
+  baseURL: "https://moodbackend-w2s8.onrender.com/api", // ✅ Your deployed backend
 });
 
 API.interceptors.request.use((req: any) => {
   const token = localStorage.getItem("token");
   if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    req.headers.Authorization = `Bearer token`;
   }
   return req;
 });
