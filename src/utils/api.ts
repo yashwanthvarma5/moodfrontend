8d8c4c6dfee171e188f9e8053a59cab58bec7adc
yashwanthvarma5 <118748767+yashwanthvarma5@users.noreply.github.com>
2025-07-23 10:06:40 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://moodbackend-w2s8.onrender.com/api",
 });
 
 API.interceptors.request.use((req: any) => {
